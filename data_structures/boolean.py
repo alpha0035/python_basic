1a4817e49e889188d1fa0c063ef2a8d0d27b96f4
Alpha <alpha07eg@gmail.com>
2025-02-04 20:17:05 +0700

    boolean datatype

---
+++ data_structures/boolean.py
@@ -0,0 +1,18 @@
+# represent in one of two value True or False
+
+# Evalute values and expression
+# bool() is used to convert a vlaue or expression into its corresponding boolean value
+x = None
+print(bool(x))
+x = 0
+print(bool(x))
+x = 0.0
+print(bool(x))
+x = ""
+print(bool(x))
+x = ()
+print(bool(x))
+x = []
+print(bool(x))
+x = {}
+print(bool(x))
