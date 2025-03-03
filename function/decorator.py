5b6a9575a7c03caa746bfad56028094e1ed4cc91
Alpha <alpha07eg@gmail.com>
2025-03-03 23:25:58 +0700

    decorator in python

--- function/decorator.py
+++ function/decorator.py
@@ -1,7 +1,21 @@
-# decorators are a powerful and flexible way to modify or extend the behavior of functions or methods, 
-# without changing their actual code.
-# Decorators are often used in scenarios such as logging, authentication and memorization
-# Sytax:
+# A decorator is a function that
+#   - Take another function as arg
+#   - Return a new function with enhanced functionality
+# -> Decorator allow us to add additional functionality to existing function or method in a clean way.
+def f1(func):
+    def wrapper():
+        print("Before calling the function.")
+        func()
+        print("After calling the function.")
+    return wrapper
+
+@f1 # shorthand for greet = f1(greet)
+def greet():
+    print("Hello, World!")
+greet()
+
+#-------------------------------------#
+# Syntax:
 # def decorator_name(func):
 #     def wrapper(*args, **kwargs):
 #         # Add functionality before the original function call
@@ -10,12 +24,12 @@
 #         return result
 #     return wrapper
 
-
 # @decorator_name
 # def function_to_decorate():
 #     # Original function code
 #     pass
 
+#-------------------------------------#
 # 1.decorator_name(func):
 # decorator_name: This is the name of the decorator function.
 # func: This parameter represents the function being decorated. When you use a decorator, the decorated function is passed to this parameter.
@@ -26,3 +40,49 @@
 # The wrapper function allows the decorator to handle functions with any number and types of arguments.
 # 3. @decorator_name:
 # This syntax applies the decorator to the function_to_decorate function. It is equivalent to writing function_to_decorate = decorator_name(function_to_decorate).
+#-------------------------------------#
+
+# Role in Decorators
+# Decorator is type of higher-order function:
+    # - Take a function as input
+    # - Modify an input function
+    # - Return a new function that extends or changes it behaviours.
+#-------------------------------------#
+
+# Type of decorators
+# 1. Function Decorators
+def deco_func(alp):
+    def bta_wrapper():
+        alp()
+    return bta_wrapper
+@deco_func
+def get():
+    print("Hello!")
+get()
+
+# 2. Method Decorators
+def method_decorator(func):
+    def wrapper(self, *args, **kwargs):
+        print("Before method execution")
+        res = func(self, *args, **kwargs)
+        print("After method execution")
+        return res
+    return wrapper
+
+class MyClass:
+    @method_decorator
+    def say_hello(self):
+        print("Hello!")
+
+obj = MyClass()
+obj.say_hello()
+# 3. Class Decorators
+def fun(cls):
+    cls.class_name = cls.__name__
+    return cls
+
+@fun
+class Person:
+    pass
+
+print(Person.class_name) 
